4ecc18c0fb50d5ec3bf000a164dbb6db1c1a0f35
Daron <daronbaltazar@gmail.com>
2024-11-26 10:26:11 -0600

    issues
    issues

--- Assets/_GAME_/Spawner/Tests/EnemyHealthTest.cs
+++ Assets/_GAME_/Spawner/Tests/EnemyHealthTest.cs
@@ -13,14 +13,14 @@ public class EnemyHealthTest
         spawner = Object.Instantiate(Resources.Load<GameObject>("Spawner"));
 
         // Ensure the prefab has the Spawner_health component
-       // Assert.IsNotNull(spawner.GetComponent<Spawner_health>(), "The spawner prefab must have the Spawner_health script.");
+        Assert.IsNotNull(spawner.GetComponent<Spawner_health>(), "The spawner prefab must have the Spawner_health script.");
     }
 
     [Test]
     public void SpawnerHealthTest()
     {
         // Act: Get Spawner health from the Spawner_health component
-       // float result = spawner.GetComponent<Spawner_health>().GetEnemyHealth();
+        float result = spawner.GetComponent<Spawner_health>().GetEnemyHealth();
 
         
         Assert.AreEqual(100, result, "The enemy health should initially be 100.");
